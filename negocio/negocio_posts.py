ad0a3e80d957a88e38a4f94ac57322c84fe25f1c
MarceloDh <marceloducommunh@gmail.com>
2025-12-10 16:47:29 -0300

    3

--- negocio/negocio_posts.py
+++ negocio/negocio_posts.py
@@ -6,36 +6,52 @@ from servicios.api_data import ClienteAPI
 cliente = ClienteAPI()
 
 def sincronizar_posts_api_db():
-    """
-    Trae los posts de la API (JSON) y los guarda en la base de datos local (SQL).
-    Cumple con: 'Los datos obtenidos se almacenarán en base de datos local'
-    """
     print("--- Sincronizando datos con JSONPlaceholder ---")
     lista_json = cliente.obtener_posts()
     
     contador = 0
-    for p_json in lista_json:
-        nuevo_post = Post(
-            id=p_json['id'],
-            userId=p_json['userId'],
-            title=p_json['title'],
-            body=p_json['body']
-        )
-        if insertar_objeto(nuevo_post):
-            contador += 1
+    if lista_json:
+        for p_json in lista_json:
+            nuevo_post = Post(
+                id=p_json['id'],
+                userId=p_json['userId'],
+                title=p_json['title'],
+                body=p_json['body']
+            )
+            if insertar_objeto(nuevo_post):
+                contador += 1
             
     print(f"Proceso terminado. Se han guardado {contador} posts nuevos.")
 
 def listar_posts_locales():
-    """Muestra los posts que estan guardados en tu PC"""
     posts = obtener_todos_los_posts()
     print(f"\n--- Listando {len(posts)} Posts Locales ---")
+    
+    if not posts:
+        print("No hay posts guardados en la base de datos local.")
+        return
+
     for p in posts:
-        print(f"[{p.id}] {p.title[:50]}...")
+        titulo_corto = (p.title[:50] + '..') if len(p.title) > 50 else p.title
+        print(f"[{p.id}] {titulo_corto}")
 
 def crear_post_nuevo(titulo, cuerpo, user_id):
     payload = {"title": titulo, "body": cuerpo, "userId": user_id}
-    return cliente.enviar_post(payload)
+    
+    codigo, respuesta_api = cliente.enviar_post(payload)
+    
+    if codigo in [200, 201]:
+        print("API respondió correctamente. Guardando respaldo local...")
+        
+        nuevo_post_local = Post(
+            userId=respuesta_api['userId'],
+            title=respuesta_api['title'],
+            body=respuesta_api['body']
+        )
+        
+        insertar_objeto(nuevo_post_local)
+        
+    return codigo, respuesta_api
 
 def modificar_post_existente(id_post, nuevo_titulo):
     payload = {"id": id_post, "title": nuevo_titulo}
